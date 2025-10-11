d4cef6b8ddadbee9ca52c8e10be709afbac335a9
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-10-11 12:48:30 +0530

    151

---
+++ Atoz/151.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/reverse-words-in-a-string/
+
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        int left = 0;
+        int right = s.length() - 1;
+
+        // Trim leading and trailing spaces
+        while (left <= right && s[left] == ' ')
+            left++;
+        while (right >= left && s[right] == ' ')
+            right--;
+
+        string temp = "";
+        string ans = "";
+
+        while (left <= right)
+        {
+            char ch = s[left];
+
+            if (ch != ' ')
+            {
+                temp += ch;
+            }
+            else
+            {
+                if (!temp.empty())
+                {
+                    if (!ans.empty())
+                        ans = temp + " " + ans;
+                    else
+                        ans = temp;
+                    temp = "";
+                }
+            }
+            left++;
+        }
+
+        // Add the last word
+        if (!temp.empty())
+        {
+            if (!ans.empty())
+                ans = temp + " " + ans;
+            else
+                ans = temp;
+        }
+
+        return ans;
+    }
+};

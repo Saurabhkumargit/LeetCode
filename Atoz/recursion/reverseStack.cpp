1f9a272e0758eac79c46850e13d95c35d36638f6
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-11-03 09:12:28 +0530

    reverse a stack

---
+++ Atoz/recursion/reverseStack.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    void insertAtBottom(stack<int> &st, int val)
+    {
+        if (st.empty())
+        {
+            st.push(val);
+            return;
+        }
+
+        int topVal = st.top();
+        st.pop();
+
+        insertAtBottom(st, val);
+
+        st.push(topVal);
+    }
+
+    void reverseStack(stack<int> &st)
+    {
+        // Your code goes here
+        if (st.empty())
+            return;
+
+        int topVal = st.top();
+        st.pop();
+
+        reverseStack(st);
+
+        insertAtBottom(st, topVal);
+    }
+};

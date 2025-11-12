bc4df2dc58a85ff8b5e14a4c10383e348bbc59cb
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-11-12 10:45:29 +0530

    90

---
+++ Atoz/recursion/90.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/subsets-ii/
+
+class Solution
+{
+public:
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(), nums.end());
+        findSubsets(0, nums, ds, ans);
+        return ans;
+    }
+
+    void findSubsets(int ind, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans)
+    {
+        ans.push_back(ds); // add current subset
+
+        for (int i = ind; i < nums.size(); i++)
+        {
+            // skip duplicates
+            if (i != ind && nums[i] == nums[i - 1])
+                continue;
+
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
+        }
+    }
+};

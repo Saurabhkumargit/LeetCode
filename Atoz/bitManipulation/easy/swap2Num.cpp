05a172198c0e85de243d58c9ce11dcc9f7d901cb
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-11-17 18:12:29 +0530

    swap2num

---
+++ Atoz/bitManipulation/easy/swap2Num.cpp
@@ -0,0 +1,13 @@
+// Swap Two Numbers
+
+class Solution
+{
+public:
+    void swap(int &a, int &b)
+    {
+        // Your code goes here
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+};

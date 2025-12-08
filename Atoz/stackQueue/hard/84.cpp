8997f6ef5eaf0aa54bd8e8d94a5f57982e08355e
saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-12-08 11:24:46 +0530

    84

---
+++ Atoz/stackQueue/hard/84.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && h < heights[st.top()]) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int right = i;
+                int left = st.empty() ? -1 : st.top();
+
+                int width = right - left - 1;
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};

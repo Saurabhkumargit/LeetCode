6156d23d892f1bb0bf04b12101de6fb2a4cfaff5
saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-12-11 12:45:52 +0530

    901.cpp

--- Atoz/stackQueue/medium/901.cpp
+++ Atoz/stackQueue/medium/901.cpp
@@ -6,19 +6,19 @@ public:
 
     StockSpanner() {}
 
-//     int next(int price) {
-//         int span = 1;
+    int next(int price) {
+        int span = 1;
 
-//         while (!st.empty() && st.top().first <= price) {
-//             span += st.top().second;
-//             st.pop();
-//         }
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
 
-//         st.push({price, span});
+        st.push({price, span});
 
-//         return span;
-//     }
-// };
+        return span;
+    }
+};
 
 
 /**

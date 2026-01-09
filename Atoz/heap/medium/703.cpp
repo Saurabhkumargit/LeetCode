9739f49ccc1e54eca3495c985b77ab7cafcfffb5
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-01-09 08:08:25 +0530

    703

---
+++ Atoz/heap/medium/703.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+public:
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int n : nums) {
+            minHeap.push(n);
+            if (minHeap.size() > k)
+                minHeap.pop();
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if (minHeap.size() > k)
+            minHeap.pop();
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */

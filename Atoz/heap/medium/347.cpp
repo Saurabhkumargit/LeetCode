858a0e338d5a3b234ecbfa859a0ddd0dc50bb4d1
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-01-10 14:22:15 +0530

    347

---
+++ Atoz/heap/medium/347.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        for(int x : nums) freq[x]++;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        for (auto &it : freq) {
+            pq.push({it.second, it.first});
+            if ((int)pq.size() > k) pq.pop();
+        }
+
+        vector<int> ans;
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

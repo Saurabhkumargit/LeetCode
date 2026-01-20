033bcd8971b6b13d185b1d55f9e0568c2216a990
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-01-20 14:22:40 +0530

    435

---
+++ Atoz/greedy/medium/435.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/non-overlapping-intervals/
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return 0;
+
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int removals = 0;a
+        int prevEnd = intervals[0][1];
+
+        for (int i = 1; i < (int)intervals.size(); i++) {
+            if (intervals[i][0] < prevEnd) {
+                removals++;
+            } else {
+                prevEnd = intervals[i][1]; 
+            }
+        }
+
+        return removals;
+    }
+};

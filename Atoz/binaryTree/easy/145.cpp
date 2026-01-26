73af731df8ee262adb9d1ee11d6b3053105d1ebc
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-01-26 16:13:47 +0530

    145

---
+++ Atoz/binaryTree/easy/145.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* node, vector<int>& result) {
+        if(!node) return;
+        dfs(node->left, result);
+        dfs(node->right, result);
+        result.push_back(node->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+};

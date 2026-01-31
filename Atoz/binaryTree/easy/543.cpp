f51a061f2c0ca7a551d5dd968c2b8908ae2032ac
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-01-31 12:40:03 +0530

    543

---
+++ Atoz/binaryTree/easy/543.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameter = 0;
+
+    int depth(TreeNode* node) {
+        if (!node) return 0;
+
+        int left = depth(node->left);
+        int right = depth(node->right);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        depth(root);
+        return diameter;
+    }
+};

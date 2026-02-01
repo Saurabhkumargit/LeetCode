0ac28b9bfda823c3d07e5336efe4a473d34b0d82
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-02-01 16:51:18 +0530

    987

---
+++ Atoz/binaryTree/hard/987.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        
+        queue<pair<TreeNode*, pair<int,int>>> q;
+        q.push({root, {0, 0}});
+        while (!q.empty()) {
+            auto p = q.front();
+            q.pop();
+
+            TreeNode* node = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            nodes[col][row].insert(node->val);
+
+            if (node->left)
+                q.push({node->left, {row + 1, col - 1}});
+            if (node->right)
+                q.push({node->right, {row + 1, col + 1}});
+        }
+
+        vector<vector<int>> result;
+
+        for (auto& [col, rows] : nodes) {
+            vector<int> column;
+            for (auto& [row, values] : rows) {
+                column.insert(column.end(), values.begin(), values.end());
+            }
+            result.push_back(column);
+        }
+
+        return result;
+    }
+};

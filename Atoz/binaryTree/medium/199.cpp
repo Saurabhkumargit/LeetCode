ffa1d6bf762f4be2c60da35fd7af01ad01bd1703
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-02-02 12:26:31 +0530

    199

---
+++ Atoz/binaryTree/medium/199.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* node, int depth, vector<int>& result) {
+        if (!node) return;
+
+        if (depth == result.size()) {
+            result.push_back(node->val);
+        }
+
+        dfs(node->right, depth + 1, result);
+        dfs(node->left, depth + 1, result);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        dfs(root, 0, result);
+        return result;
+    }
+};

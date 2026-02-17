8de90b1f636ccc308b811f637b5cd59edea496ff
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-02-17 11:43:21 +0530

    94

---
+++ Atoz/binaryTree/hard/94.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        TreeNode* curr = root;
+
+        while (curr) {
+            if (!curr->left) {
+                result.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* pred = curr->left;
+                while (pred->right && pred->right != curr) {
+                    pred = pred->right;
+                }
+
+                if (!pred->right) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    pred->right = nullptr;
+                    result.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return result;
+    }
+};

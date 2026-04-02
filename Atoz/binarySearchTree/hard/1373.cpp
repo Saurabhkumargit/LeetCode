edfaa03eb70d07cdc185d9ede753d8633bbc8d58
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-02 13:58:25 +0530

    1373

---
+++ Atoz/binarySearchTree/hard/1373.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/maximum-sum-bst-in-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxSum = 0;
+
+    struct Info {
+        bool isBST;
+        int minVal, maxVal, sum;
+    };
+
+    Info dfs(TreeNode* root) {
+        if (!root) {
+            return {true, INT_MAX, INT_MIN, 0};
+        }
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.isBST && right.isBST && 
+            root->val > left.maxVal && root->val < right.minVal) {
+            
+            int currSum = left.sum + right.sum + root->val;
+            maxSum = max(maxSum, currSum);
+
+            return {
+                true,
+                min(root->val, left.minVal),
+                max(root->val, right.maxVal),
+                currSum
+            };
+        }
+
+        return {false, INT_MIN, INT_MAX, 0};
+    }
+
+    int maxSumBST(TreeNode* root) {
+        dfs(root);
+        return maxSum;
+    }
+};

2f2bcfce8024b5fcb7bebaaa9c5b83369d18a428
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-08 15:13:34 +0530

    560

--- Atoz/array/medium/560.cpp
+++ Atoz/array/medium/560.cpp
@@ -16,3 +16,23 @@ public:
             return cnt;
     }
 };
+
+// Revision (1)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> prefix;
+        int cnt = 0;
+        int sum = 0;
+        prefix[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int req = sum - k;
+            if (prefix.find(req) != prefix.end()) {
+                cnt += prefix[req];
+            }
+            prefix[sum] += 1;
+        }
+        return cnt;
+    }
+};

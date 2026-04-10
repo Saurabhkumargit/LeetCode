50219d9530c1954d8dd947277724a5382fc4907b
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-10 14:12:30 +0530

    3

---
+++ Atoz/string/medium/3.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> mp;
+        int l = 0;
+        int maxLen = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            if (mp.find(s[r]) != mp.end()) {
+                l = max(l, mp[s[r]] + 1);
+            }
+
+            mp[s[r]] = r;
+            maxLen = max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+};

70c3274b7d1655866b31c481bf53c99bf5d5a079
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-10 12:26:12 +0530

    1

--- Atoz/array/easy/1.cpp
+++ Atoz/array/easy/1.cpp
@@ -15,3 +15,21 @@ public:
         return {};
     }
 };
+
+
+// revision (1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i <nums.size(); i++) {
+            int pair = target - nums[i];
+            if (mp.find(pair) != mp.end()) {
+                return {mp[pair], i};
+            } else {
+                mp[nums[i]] = i;
+            }
+        }
+        return {-1, -1};
+    }
+};

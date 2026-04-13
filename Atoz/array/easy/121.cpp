4bdb68bb07bc8cfbfcfece33e5f43255fca41724
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-04-13 14:20:41 +0530

    121

--- Atoz/array/easy/121.cpp
+++ Atoz/array/easy/121.cpp
@@ -17,3 +17,24 @@ public:
         return profit;
     }
 };
+
+
+// Revision(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        int min = prices[0];
+        int profit = 0;
+        for ( int i =0; i < prices.size(); i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (prices[i] - min > profit){
+                profit = prices[i] - min;
+            }
+
+        }
+        return profit;
+    }
+};

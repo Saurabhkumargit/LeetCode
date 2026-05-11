4c60cca7e25b6832ce419020240bab6adf933670
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-05-11 18:10:09 +0530

    53

--- Atoz/array/medium/53.cpp
+++ Atoz/array/medium/53.cpp
@@ -36,3 +36,22 @@ public:
 
     }
 };
+
+
+// Revision(2)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            maxSum = max(sum, maxSum);
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+};

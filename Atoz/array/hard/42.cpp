26b1c91f61b8e90c901babf99d38d8b8ca6f5bc4
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-05-19 14:25:32 +0530

    42

--- Atoz/array/hard/42.cpp
+++ Atoz/array/hard/42.cpp
@@ -27,3 +27,36 @@ public:
         return total;
     }
 };
+
+
+// Revision (1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int total = 0;
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= leftMax) {
+                    leftMax = height[l];
+                } else {
+                    total += leftMax - height[l];
+                }
+                l++;
+            } else {
+                if (height[r] >= rightMax) {
+                    rightMax = height[r];
+                } else {
+                    total += rightMax - height[r];
+                }
+                r--;
+            }
+        }
+
+        return total;
+    }
+};

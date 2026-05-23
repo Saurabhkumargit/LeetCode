3c743eb1a57fa1d31ab1abae9f2fa94efebab832
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-05-23 16:08:52 +0530

    238

--- Atoz/array/medium/238.cpp
+++ Atoz/array/medium/238.cpp
@@ -65,3 +65,25 @@ public:
         return prd;
     }
 };
+
+
+// Revision (3)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prd(n);
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            prd[i] = prefix;
+            prefix*=nums[i];
+        }
+
+        int suffix = 1;
+        for (int i = n-1; i >= 0; i--) {
+            prd[i] *= suffix;
+            suffix*=nums[i];
+        }
+        return prd;
+    }
+};

56f889c50ffc2ab6f580896401b09650bcc4e5ac
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-06-19 17:22:16 +0530

    704

--- Atoz/binarySearch/easy/704.cpp
+++ Atoz/binarySearch/easy/704.cpp
@@ -74,3 +74,25 @@ public:
         return -1;
     }
 };
+
+
+// Revision (4)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int low = 0;
+        int high = n-1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid+1;
+            } else high = mid-1;
+        }
+        return -1;
+    }
+};

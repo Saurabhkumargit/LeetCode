74b74ea0a9986206feb4f3fac815a8ca6f9b5e7d
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-06-23 18:18:52 +0530

    242

--- Atoz/string/242.cpp
+++ Atoz/string/242.cpp
@@ -29,3 +29,26 @@ public:
         return true;
     }
 };
+
+
+// Revision (1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+        if (s.length() != t.length())
+            return false;
+
+        for (int i = 0; i< s.length(); i++) {
+            mp[s[i]]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            mp[t[i]]--;
+        }
+        for (int i = 0; i < mp.size(); i++) {
+            if (mp[s[i]] != 0) 
+                return false;
+        }
+        return true;
+    }
+};

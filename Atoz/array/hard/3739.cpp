5fd78204026b1f66e09d133130576f546ac89653
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-06-26 18:33:59 +0530

    3739

---
+++ Atoz/array/hard/3739.cpp
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/count-subarrays-with-majority-element-ii/
+
+class Solution {
+public:
+    struct Fenwick {
+        vector<int> bit;
+        int n;
+
+        Fenwick(int sz) {
+            n = sz;
+            bit.assign(n + 1, 0);
+        }
+
+        void update(int idx, int val) {
+            while (idx <= n) {
+                bit[idx] += val;
+                idx += idx & -idx;
+            }
+        }
+
+        int query(int idx) {
+            int sum = 0;
+            while (idx > 0) {
+                sum += bit[idx];
+                idx -= idx & -idx;
+            }
+            return sum;
+        }
+    };
+
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> prefix(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        vector<int> vals = prefix;
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+
+        Fenwick bit(vals.size());
+
+        long long ans = 0;
+
+        for (int x : prefix) {
+            int idx = lower_bound(vals.begin(), vals.end(), x) - vals.begin() + 1;
+
+            ans += bit.query(idx - 1);
+
+            bit.update(idx, 1);
+        }
+
+        return ans;
+    }
+};

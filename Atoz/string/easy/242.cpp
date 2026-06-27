954b86f8bbdb95a66f0365163bfbb84159c95902
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-06-27 13:44:01 +0530

    242

--- Atoz/string/easy/242.cpp
+++ Atoz/string/easy/242.cpp
@@ -52,3 +52,24 @@ public:
         return true;
     }
 };
+
+
+// Revision (2)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+
+        for (char c : s) 
+            count[c-'a']++;
+        
+        for (char c : t)
+            count[c - 'a']--;
+        
+        for (int x : count) {
+            if (x != 0)
+                return false;
+        }
+        return true;
+    }
+};

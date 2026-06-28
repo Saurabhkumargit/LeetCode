e12ab4b918b5ec8ac00d8680c65cf754bda20f44
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-06-28 10:59:39 +0530

    49

--- Atoz/array/medium/49.cpp
+++ Atoz/array/medium/49.cpp
@@ -80,3 +80,29 @@ public:
         return ans;
     }
 };
+
+
+// Revision (2)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (int i = 0; i < strs.size(); i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            string key = s;
+            if (mp.find(key) != mp.end()) {
+                mp[key].push_back(strs[i]);
+            } else {
+                mp[key].push_back(strs[i]);
+            }
+        }
+
+        vector<vector<string>> ans;
+        for (auto &it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};

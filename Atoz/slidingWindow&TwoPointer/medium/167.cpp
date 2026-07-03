fadaa5a48145c37ad4a11e8e118c87056577ce4c
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-03 16:21:23 +0530

    167

--- Atoz/slidingWindow&TwoPointer/medium/167.cpp
+++ Atoz/slidingWindow&TwoPointer/medium/167.cpp
@@ -21,3 +21,26 @@ public:
         return {};
     }
 };
+
+
+// Revision (1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int st = 0;
+        int end = numbers.size() -1;
+
+        while (end > st) {
+            int sum = numbers[st] + numbers[end];
+
+            if (sum == target) {
+                return {st +1, end +1};
+            } else if (sum < target) {
+                st++;
+            } else {
+                end--;
+            }
+        }
+        return {-1, -1};
+    }
+};

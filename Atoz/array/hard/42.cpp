1fe83bb018678b88571a91b242d6a08d7100f21c
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-07 14:36:09 +0530

    42

--- Atoz/array/hard/42.cpp
+++ Atoz/array/hard/42.cpp
@@ -123,3 +123,35 @@ public:
         return water;
     }
 };
+
+
+// Revision (4)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int lmax = 0, rmax = 0;
+        int ans = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= lmax)
+                    lmax = height[left];
+                else
+                    ans += lmax - height[left];
+
+                left++;
+            } else {
+                if (height[right] >= rmax)
+                    rmax = height[right];
+                else
+                    ans += rmax - height[right];
+
+                right--;
+            }
+        }
+
+        return ans;
+    }
+};

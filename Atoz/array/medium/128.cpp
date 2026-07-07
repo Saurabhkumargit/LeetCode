559d10405e69b49139c3acf28db8547eff3dbb09
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-07 14:10:43 +0530

    128

--- Atoz/array/medium/128.cpp
+++ Atoz/array/medium/128.cpp
@@ -72,3 +72,28 @@ public:
         return longest;
     }
 };
+
+
+// Revision (3)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        int longest = 0;
+
+        for (int num : st) {
+            if (!st.count(num - 1)) {
+                int curr = num;
+                int len = 1;
+
+                while (st.count(curr + 1)) {
+                    curr++;
+                    len++;
+                }
+
+                longest = max(longest, len);
+            }
+        }
+        return longest;
+    }
+};

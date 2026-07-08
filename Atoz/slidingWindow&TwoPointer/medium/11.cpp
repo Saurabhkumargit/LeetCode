1ae0dc1305f0abf1a09e5e19cd6323a0a9323ccc
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-08 14:06:15 +0530

    11

--- Atoz/slidingWindow&TwoPointer/medium/11.cpp
+++ Atoz/slidingWindow&TwoPointer/medium/11.cpp
@@ -20,3 +20,26 @@ public:
         return maxWater;
     }
 };
+
+
+// Revision (1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int ans = 0;
+
+        while (l < r) {
+            int area = min(height[l], height[r]) * (r - l);
+            ans = max(ans, area);
+
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+
+        return ans;
+    }
+};

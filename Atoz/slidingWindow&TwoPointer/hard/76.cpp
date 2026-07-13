ea18fe7e401d3911882454bce6d4ac8369902de7
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-13 20:56:35 +0530

    76

--- Atoz/slidingWindow&TwoPointer/hard/76.cpp
+++ Atoz/slidingWindow&TwoPointer/hard/76.cpp
@@ -87,3 +87,50 @@ public:
         return minLen == INT_MAX ? "" : s.substr(start, minLen);
     }
 };
+
+
+// Revision (2)
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (t.size() > s.size()) return "";
+
+        unordered_map<char, int> need;
+        unordered_map<char, int> window;
+
+        for (char c : t)
+            need[c]++;
+
+        int have = 0;
+        int needCount = need.size();
+
+        int left = 0;
+        int minLen = INT_MAX;
+        int start = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            char c = s[right];
+            window[c]++;
+
+            if (need.count(c) && window[c] == need[c])
+                have++;
+
+            while (have == needCount) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char ch = s[left];
+                window[ch]--;
+
+                if (need.count(ch) && window[ch] < need[ch])
+                    have--;
+
+                left++;
+            }
+        }
+
+        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    }
+};

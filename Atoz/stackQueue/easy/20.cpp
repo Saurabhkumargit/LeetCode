9fe9040dc47050406ce57db5b0cdd9b1bbc7b740
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-14 16:19:47 +0530

    20

--- Atoz/stackQueue/easy/20.cpp
+++ Atoz/stackQueue/easy/20.cpp
@@ -97,3 +97,34 @@ public:
         return st.empty();
     }
 };
+
+
+// Revision (3)
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (!st.empty() && s[i] == ')' && st.top() == '(') {
+                st.pop();
+                continue;
+            }
+
+            if (!st.empty() && s[i] == '}' && st.top() == '{') {
+                st.pop();
+                continue;
+            }
+
+            if (!st.empty() && s[i] == ']' && st.top() == '[') {
+                st.pop();
+                continue;
+            }
+
+            st.push(s[i]);
+        }
+
+        return st.empty();
+    }
+};

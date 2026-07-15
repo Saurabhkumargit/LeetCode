338b87b8f53c9a4c223c6204e3f2135924fac790
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-15 15:55:34 +0530

    3658

---
+++ Atoz/streak questions/3658.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 1; i <= n; i++) {
+            odd += 2 * i - 1;
+            even += 2 * i;
+        }
+
+        return __gcd(odd, even);
+    }
+};

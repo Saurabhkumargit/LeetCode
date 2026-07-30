4c3463f4f99a12082fb978916e4d7611e78802ec
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-30 13:03:07 +0530

    1757

---
+++ Atoz/sql/select/easy/1757.sql
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+# Write your MySQL query statement below
+SELECT product_id
+FROM products
+WHERE low_fats = 'Y'
+    AND recyclable = 'Y'

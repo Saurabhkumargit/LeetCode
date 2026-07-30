b4b79fb211a87215a0fe0c2fcff8a67a6db1767a
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-30 16:01:42 +0530

    1633

---
+++ Atoz/sql/basicAggFun/easy/1633.sql
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/percentage-of-users-attended-a-contest/
+
+# Write your MySQL query statement below
+SELECT 
+    contest_id,
+    ROUND(
+        COUNT(user_id) * 100 /
+        (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC,
+         contest_id ASC;

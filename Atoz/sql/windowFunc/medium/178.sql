2eab7f02d9bea0790ceab1f2b7be392076de0c5b
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-07-30 17:14:44 +0530

    178

---
+++ Atoz/sql/windowFunc/medium/178.sql
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/rank-scores/
+
+# Write your MySQL query statement below
+SELECT 
+    score,
+    DENSE_RANK() OVER (
+        ORDER BY score DESC
+    ) AS `rank`
+FROM Scores;

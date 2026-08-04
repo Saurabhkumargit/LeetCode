b376c307404c993bf00a3c5bce97b9f0c81f8b46
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-08-04 13:13:13 +0530

    19

--- Atoz/linkedList/medium/19.cpp
+++ Atoz/linkedList/medium/19.cpp
@@ -43,3 +43,45 @@ public:
         return newHead;
     }
 };
+
+
+// Revision (1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* curr = head;
+        int length = 0;
+        while(curr) {
+            length++;
+            curr = curr->next; 
+        }
+
+        if (n == length) {
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+
+        curr = head;
+        for (int i = 1; i < length - n; i++) {
+            curr = curr->next;
+        }
+
+        ListNode* temp = curr->next;
+        curr->next = temp->next;
+        delete temp;
+
+        return head;
+    }
+};

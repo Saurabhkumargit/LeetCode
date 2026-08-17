5b9cf2f0a237345b6cf0e0bcdffa018498e546c8
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-08-17 14:04:25 +0530

    105

--- Atoz/binaryTree/medium/105.cpp
+++ Atoz/binaryTree/medium/105.cpp
@@ -40,3 +40,48 @@ public:
         return build(preorder, 0, inorder.size() - 1);
     }
 };
+
+
+// Revision (1)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int preIndex = 0;
+
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return nullptr;
+        }
+
+        int rootValue = preorder[preIndex];
+        preIndex++;
+
+        TreeNode* root = new TreeNode(rootValue);
+
+        int rootIndex = inStart;
+
+        while (inorder[rootIndex] != rootValue) {
+            rootIndex++;
+        }
+
+        root->left = build(preorder, inorder, inStart, rootIndex - 1);
+
+        root->right = build(preorder, inorder, rootIndex +1, inEnd);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return build(preorder, inorder, 0, inorder.size() - 1);
+    }
+};

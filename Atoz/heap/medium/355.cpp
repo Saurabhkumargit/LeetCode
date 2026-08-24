4f500bc0b85d1c7015dd4dcfbdd86064dd07f23a
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-08-24 13:02:30 +0530

    355

--- Atoz/heap/medium/355.cpp
+++ Atoz/heap/medium/355.cpp
@@ -63,3 +63,84 @@ public:
  * obj->follow(followerId,followeeId);
  * obj->unfollow(followerId,followeeId);
  */
+
+
+ // Revision (1)
+ class Twitter {
+public:
+    unordered_map<int, unordered_set<int>> following;
+    unordered_map<int, vector<pair<int, int>>> tweets;
+
+    int time = 0;
+    
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({time++, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        using T = tuple<int, int, int, int>;
+        priority_queue<T> pq;
+
+        if(tweets.count(userId) && !tweets[userId].empty()) {
+            int idx = tweets[userId].size() -1;
+            auto [t, id] = tweets[userId][idx];
+            pq.push({t, id, userId, idx});
+        }
+
+        for (int followee : following[userId]) {
+            if (!tweets.count(followee) || tweets[followee].empty())
+                continue;
+            
+            int idx = tweets[followee].size() - 1;
+            auto[t, id] = tweets[followee][idx];
+            pq.push({t, id, followee, idx});
+        }
+
+        vector<int> feed;
+
+        while (!pq.empty() && feed.size() < 10) {
+            auto [t, tweetId, author, idx] = pq.top();
+            pq.pop();
+
+            feed.push_back(tweetId);
+
+            if (idx > 0) {
+                int nextIdx = idx - 1;
+                auto [nextTime, nextTweetId] = tweets[author][nextIdx];
+
+                pq.push({
+                    nextTime,
+                    nextTweetId,
+                    author,
+                    nextIdx
+                });
+            }
+        }
+
+        return feed;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if (followerId == followeeId)
+            return;
+        
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        following[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */

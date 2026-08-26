be9b61eaba9c617cae7c836651e23d8b13212853
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-08-26 12:12:39 +0530

    46

---
+++ Atoz/recursion/medium/46.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/permutations/
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> current;
+
+    void backtrack(vector<int>& nums, vector<bool>& used) {
+        if (current.size() == nums.size()) {
+            ans.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i])
+                continue;
+
+            used[i] = true;
+            current.push_back(nums[i]);
+
+            backtrack(nums, used);
+
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+
+        backtrack(nums, used);
+
+        return ans;
+    }
+};

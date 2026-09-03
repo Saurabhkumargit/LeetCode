566c02c031a144f9acd4490284cb914a1cf83ccf
saurabhkumargit <saurabhkumar.t21@gmail.com>
2026-09-03 13:26:43 +0530

    17

--- Atoz/recursion/medium/17.cpp
+++ Atoz/recursion/medium/17.cpp
@@ -45,3 +45,54 @@ public:
         return ans;
     }
 };
+
+
+// Revision (1)
+class Solution {
+public:
+    vector<string> ans;
+
+    void solve(int idx, string& digits,
+               unordered_map<char, string>& mp,
+               string& curr) {
+
+        if (curr.size() == digits.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        char ch = digits[idx];
+        string str = mp[ch];
+
+        for (int i = 0; i < str.length(); i++) {
+            curr.push_back(str[i]);
+
+            solve(idx + 1, digits, mp, curr);
+
+            curr.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+
+        if (digits.empty())
+            return {};
+
+        unordered_map<char, string> mp = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"}
+        };
+
+        string curr = "";
+
+        solve(0, digits, mp, curr);
+
+        return ans;
+    }
+};
